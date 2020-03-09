ad78759e5d0bd8267b019d993cdb73933cf28749
thierry vinagre <vinagre.thierry@yahoo.fr>
2020-03-09 15:03:00 +0100

    Finished ok

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -6,6 +6,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_dispatch.show_exceptions = true # Update this line
+
   config.cache_classes = false
 
   # Do not eager load code on boot. This avoids loading your whole application
